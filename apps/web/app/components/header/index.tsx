f06151c5b3f7e82f17397682d59a76df35672808
Nex <misanthropicnex@gmail.com>
2026-01-16 06:23:51 +0000

    Enhance customization settings with font and theme selection
    - Added font and theme selection options in the customization settings.
    - Integrated Select components for user-friendly font and theme choices.
    - Updated layout to improve accessibility and user experience in settings.

--- apps/web/app/components/header/index.tsx
+++ apps/web/app/components/header/index.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
     <header className="relative flex items-center justify-between pt-1 px-2">
       <SidebarTrigger />
       <HeaderBreadcrumb className="absolute left-1/2 -translate-x-1/2 overflow-hidden" />
-      <div className="flex items-center justify-center gap-4 hidden">
+      <div className="items-center justify-center gap-4 hidden md:flex">
         <Cmdk />
         <Help />
         <FontToggle />
