cf9400cb85814c2270216095bbfc8d35661288b3
Nex <misanthropicnex@gmail.com>
2026-01-28 11:21:15 +0000

    Add env.ts file for centralized environment variable management; define client and experimental runtime environment variables for web application URLs.

---
+++ apps/web/www/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_WEB_APP_URL: z.string().url().optional(),
+    NEXT_PUBLIC_WEB_BLOG_URL: z.string().url().optional(),
+    NEXT_PUBLIC_WEB_HELP_URL: z.string().url().optional(),
+    NEXT_PUBLIC_WEB_STATUS_URL: z.string().url().optional(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_WEB_APP_URL: process.env.NEXT_PUBLIC_WEB_APP_URL,
+    NEXT_PUBLIC_WEB_BLOG_URL: process.env.NEXT_PUBLIC_WEB_BLOG_URL,
+    NEXT_PUBLIC_WEB_HELP_URL: process.env.NEXT_PUBLIC_WEB_HELP_URL,
+    NEXT_PUBLIC_WEB_STATUS_URL: process.env.NEXT_PUBLIC_WEB_STATUS_URL,
+  },
+});
