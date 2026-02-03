ec613ae08f979c613cdc2cc31d93ef3453860aca
Nex <misanthropicnex@gmail.com>
2026-02-03 05:09:17 +0000

    Update AuthLayout component to improve responsiveness by adjusting layout classes for left and right sections, ensuring proper display on medium and larger screens.

--- apps/web/app/app/(auth)/layout.tsx
+++ apps/web/app/app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({
       <Meteors maxDuration={25} number={100} />
 
       {/* Left Side */}
-      <div className="flex items-center justify-center w-1/2 bg-sidebar">
+      <div className="hidden md:flex items-center justify-center w-1/2 bg-sidebar">
         <div className="flex gap-2 items-center z-100">
           <Command className="size-28" />
           <div className="flex flex-col">
@@ -24,7 +24,7 @@ export default function AuthLayout({
       </div>
 
       {/* Right Side */}
-      <div className="z-100 w-1/2 bg-background border shadow-xl rounded-xl flex overflow-y-auto no-scrollbar">
+      <div className="z-100 w-full md:w-1/2 bg-background border shadow-xl rounded-xl flex overflow-y-auto no-scrollbar">
         <div className="m-auto py-8">{children}</div>
       </div>
     </div>
