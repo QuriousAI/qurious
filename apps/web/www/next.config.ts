77d2d3462a93725842853ce0122fd0b6df26e031
Nex <misanthropicnex@gmail.com>
2026-02-16 10:57:59 +0000

    Update Next.js configuration to allow production builds with type errors by enabling ignoreBuildErrors option

--- apps/web/www/next.config.ts
+++ apps/web/www/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 import "./env";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
