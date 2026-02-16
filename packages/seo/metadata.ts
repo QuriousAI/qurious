b1cee89d3eff57e527a5daedf680a787764032f0
Nex <misanthropicnex@gmail.com>
2026-02-16 06:51:36 +0000

    Add SEO package with metadata generation functionality and update dependencies

---
+++ packages/seo/metadata.ts
@@ -0,0 +1,69 @@
+import type { Metadata } from "next";
+
+type MetadataGenerator = Omit<Metadata, "description" | "title"> & {
+  title: string;
+  description: string;
+  image?: string;
+};
+
+const applicationName = "Qurious";
+const author: Metadata["authors"] = {
+  name: "Sahil Khan",
+  url: "https://sahilll.dev/",
+};
+const publisher = "Sahil Khan";
+const twitterHandle = "@sahillll_khan";
+
+const metadataBase = new URL(`https://quriousai.xyz`);
+
+export const createMetadata = ({
+  title,
+  description,
+  image,
+  ...properties
+}: MetadataGenerator): Metadata => {
+  const parsedTitle = `${title} | ${applicationName}`;
+  const defaultMetadata: Metadata = {
+    title: parsedTitle,
+    description,
+    applicationName,
+    metadataBase,
+    authors: [author],
+    creator: author.name,
+    formatDetection: {
+      telephone: false,
+    },
+    appleWebApp: {
+      capable: true,
+      statusBarStyle: "default",
+      title: parsedTitle,
+    },
+    openGraph: {
+      title: parsedTitle,
+      description,
+      type: "website",
+      siteName: applicationName,
+      locale: "en_US",
+    },
+    publisher,
+    twitter: {
+      card: "summary_large_image",
+      creator: twitterHandle,
+    },
+  };
+
+  const metadata: Metadata = { ...defaultMetadata, ...properties };
+
+  if (image && metadata.openGraph) {
+    metadata.openGraph.images = [
+      {
+        url: image,
+        width: 1200,
+        height: 630,
+        alt: title,
+      },
+    ];
+  }
+
+  return metadata;
+};
